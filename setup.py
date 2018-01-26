6a6490e2b90a20a50b7db9a92885b096ecc01fda
Maritza EBLING <maritza@mylestone.com>
2018-01-26 14:21:52 -0500

    use openblas instead of cblas'

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if sys.platform == 'darwin':
                    sources=['tsne/bh_sne_src/quadtree.cpp', 'tsne/bh_sne_src/tsne.cpp', 'tsne/bh_sne.pyx'],
                    include_dirs=[numpy.get_include(), 'tsne/bh_sne_src/'],
                    extra_compile_args=extra_compile_args,
-                   extra_link_args=['-Wl,-framework', '-Wl,Accelerate', '-lcblas'],
+                   extra_link_args=['-Wl,-framework', '-Wl,Accelerate', '-lopenblas'],
                    language='c++')]
 else:
     extra_link_args = ['-lcblas']
